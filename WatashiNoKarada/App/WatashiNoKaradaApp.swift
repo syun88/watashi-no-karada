d1e3917efd07e1ac2c3c781dd9eb39a3ac43d591
SYUN <114974822+syun88@users.noreply.github.com>
2026-09-06 16:10:23 +0900

    Add app entry point

---
+++ WatashiNoKarada/App/WatashiNoKaradaApp.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+import SwiftData
+
+@main
+struct WatashiNoKaradaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([BodyRecord.self])
+        let config = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        do {
+            return try ModelContainer(for: schema, configurations: [config])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            AppRootView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
